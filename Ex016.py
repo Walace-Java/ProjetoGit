7470f3c5f2f3af93f7a9b1ce87ef8bfebe296aaf
Walace-Java <keystonewalace@gmail.com>
2024-08-16 00:14:51 -0300

    novos execercicios adicionados

---
+++ Ex016.py
@@ -0,0 +1,6 @@
+'''from math import trunc
+num = float(input('digite o valor'))
+print('O valor digitado foi {} e a sua porção interia é {}'.format(num, trunc(num)))
+'''
+num  = float(input('digite um valor'))
+print('o valor digitado foi {} arredondado fica {}'.format(num, int(num)))
